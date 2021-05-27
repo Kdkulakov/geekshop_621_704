40b9ab4204c513fdc602925f4bb2899ae35a4d70
Kulakov <kdkulakov@gmail.com>
2021-05-27 15:52:28 +0300

    add lesson 2

--- geekshop/views.py
+++ geekshop/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+import json
 
 def index(request):
-    return render(request, 'index.html')
+    title = 'geekshop'
+    list_params = ['a1', 'a2', 'a3']
+
+    context = {
+        'list_params': list_params,
+        'some_name': 'hello',
+        'title': title,
+    }
+    return render(request, 'index.html', context=context)
 
 def contacts(request):
     return render(request, 'contact.html')
