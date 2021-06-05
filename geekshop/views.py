0e0eca06ad9e13f190e1395952d7e41a44a77a97
Kulakov <kdkulakov@gmail.com>
2021-06-05 09:49:06 +0300

    add lesson4

--- geekshop/views.py
+++ geekshop/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+
+from basketapp.models import Basket
 from mainapp.models import Product
 
 
 def index(request):
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
 
     title = 'geekshop'
     products = Product.objects.all()[:4]
@@ -11,6 +16,7 @@ def index(request):
         'products': products,
         'some_name': 'hello',
         'title': title,
+        'basket': basket,
     }
     return render(request, 'index.html', context=context)
 
